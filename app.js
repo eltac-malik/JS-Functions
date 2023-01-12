650f9db2e72b881ca19f73675358942c97148649
eltac-malik <eltacem@code.edu.az>
2023-01-12 22:54:04 +0400

    com-1

---
+++ app.js
@@ -0,0 +1,132 @@
+//let a = "Aziz Kerimov"
+
+// console.log(a.slice(0,2));
+// console.log(a.split(" "));
+// console.log(a.trim());
+// console.log(a.length);
+// console.log(a.toLowerCase());
+// console.log(a.toUpperCase());
+// console.log(a.indexOf("z"));
+// console.log(a.concat("salam"));
+// console.log(a.replaceAll("z","t"));
+// console.log(a.replaceAll(/a|i/g,"*"));
+// console.log(a.charAt(2));
+// console.log(a.startsWith("A"));
+// console.log(a.endsWith("v"));
+// console.log(a.repeat(2));
+// console.log(a.substring(5,1));
+// console.log(a.match("o"));
+// console.log(a.search("o"));
+// console.log(a.includes("y"));
+// console.log(a.padStart(100,"*"));
+// console.log(a.padEnd(100,"z"));
+// console.log(a.lastIndexOf("z"));
+// console.log(a.localeCompare("Aziz"));
+// console.log(a.substr(5,10));
+
+
+
+
+// HOMEWORK
+
+// TASK 1
+// POLINDROM OR NOT POLINDROM
+
+
+//let str = "1000110001221000110001";
+
+// let arr=str.split("")
+
+
+// for (let i = 0; i < arr.length/2; i++) {
+//   if (arr[i] === arr[arr.length - 1 - i]) {
+//     console.log("polindrom");
+//   }
+//     else{
+//         console.log("polindrom deyil");
+//     }
+
+// }
+
+
+// FUNCTIONS
+
+// REGULAR FUNCTIONS
+// ARROW FUNCTIONS
+
+
+// 1) REGULAR FUNCTION
+
+// 1.1) DECLARATION FUNCTION
+// 1.2) EXPRESSION FUNCTION
+
+// DECLARATION FUNCTION
+
+// function sayHello() {
+//     console.log("salam")
+// }
+
+// EXPRESSION FUNCTION
+// ANNONYMOUS FUNCTION
+
+// let myFunc = function () {
+//   console.log("salam");
+// };
+
+
+//  DECLARATION
+
+// function sayHello() {
+//   console.log("salam");
+// }
+
+
+// INVOKED
+// sayHello();
+
+// initial value name='test'
+
+// function sayMyName(name,surname='Bosdur',age) {
+//     console.log(`${name} ${surname} ${age}`);
+// }
+
+// sayMyName("Ali","Almammadov")
+// sayMyName("Eziz","Kerimov")
+
+// let expFunc = function (name, surname = "Bosdur", age) {
+//   console.log(`${name} ${surname} ${age}`);
+// };
+
+//  expFunc("Ali","Almammadov",30)
+
+
+// FUNCTION RETURN
+
+// function handleSum(num1,num2) {
+//     num1 = num1 + 1;
+//     num2 = num2*2;
+//     return num1+num2;
+// }
+
+// console.log(handleSum(5, 4));
+
+// const expFunc2 = function(name){
+//     return `adiniz ${name}`
+// }
+
+// console.log(expFunc2("Zerengul"));
+
+
+// PURE FUNCTION
+
+// function pureFunc(a) {
+//     let b = 2
+//     return a+b
+// }
+
+// console.log(pureFunc(1));
+
+
+// FUNCTIONS
+// OBJECTS
+// ARRAY
